2cb4edd4f2f3a3d43ccc5c70a2c2fe01148a2212
Niel Cnops <nielcnops@outlook.com>
2020-06-18 20:42:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def join_nested_strings(src)
   total = 0 
   while total <src.count do
     element_index = 0
-    while element_index > src.count[guessing_game].count do 
+    while element_index > src.count[guessing_game].total do 
       total += src[guessing_game][element_index]
       element_index += 1
     end
