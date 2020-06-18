84ee74c6ab2b437600374ebd1dbe21525fa921fd
Niel Cnops <nielcnops@outlook.com>
2020-06-18 22:36:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,8 @@
 def join_nested_strings(src)
-  row_index=0
-  nested_strings=0
+  row_index =0
+  nested_strings =0
   while row_index < src.count do
-    inner_index=0
+    inner_index =0
     while inner_index < src[row_index].count do
       if src[row_index][inner_index].class == "string"
          nested_strings << src[row_index][inner_index] .each(" ")
