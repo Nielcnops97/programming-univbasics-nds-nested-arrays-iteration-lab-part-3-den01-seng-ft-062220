7f2f59f56c0149483d632d9e50764cb026fa8278
Niel Cnops <nielcnops@outlook.com>
2020-06-18 21:04:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,8 +2,14 @@ def join_nested_strings(src)
   gussing_game= []
   total = 0
   row_index = 0
-  while row_index <
-
+  while row_index < gussing_game_grid.count do
+    element_index = 0
+    while element_index < gussing_game_grid[row_index].count do
+      total+= gussing_game_grid[row_index][element_index]
+      element_index += 1
+    end
+    row_index += 1
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
 end
+total
